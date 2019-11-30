aa761aee7e84894e73eab04c91bdfc084d090396
ArielBrito <arielbrit@gmail.com>
2019-11-30 18:01:18 -0400

    "Definicion de la rutas"

--- backend/models/Book.js
+++ backend/models/Book.js
@@ -4,7 +4,7 @@ const {Schema, model}=require('mongoose'); // Instanciamos el metodo para crear 
     title: {type:String, required: true},
     autor: {type: String, required: true},
     isbn: {type:String, required: true},
-    imagePath: {type:String, required: true},
+   imagePath: {type:String},
     created_at: {type: Date, default: Date.now}
 });
 
--- backend/routes/book.js
+++ backend/routes/book.js
@@ -1,9 +1,34 @@
 //Importar el modulo Router de expres para manejar las rutas
 const {Router}= require('express');
+const Book = require('../models/Book');
 
 const router= Router();
 
-router.get('/', (req, res)=> res.json({ text:'Hello World'}));
+router.get('/',async (req, res)=> {// Esta ruta muestra todos los objetos
+   const books= await Book.find();
+
+   res.json(books);
+     
+});
+
+router.post('/', async(req, res)=>{// En esta enviamos a la base de datos los objetos con los datos correspondientes
+   const {title, autor, isbn}=req.body;
+
+  const newBook= new Book({title, autor, isbn});
+  await newBook.save();
+  console.log(newBook);
+    res.json({message:'libro guardado'});
+
+
+});
+
+router.delete('/:id', async(req, res)=>{// Eliminamos un objeto
+
+   const book= await Book.findByIdAndDelete(req.params.id);
+    //console.log(req.params.id)
+    res.json({message:'Libro eliminado correctamente'});
+
+});
 
 
 module.exports = router;
